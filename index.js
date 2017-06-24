d4add46e10b4fa6302cdcaf0fde0dd5d9abace6e
CREPIN Andy <acrepinpro@gmail.com>
2017-06-24 14:50:36 +0200

    added possibility to add multiple .js and .css through array objects

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 module.exports = function (opts) {
-  var title = opts.title ? `<title>${opts.title}</title>` : ''
-  var headScript = (opts.script && opts.scriptAsync) ? `<script src="${opts.script}" async></script>` : ''
-  var bodyScript = (opts.script && !opts.scriptAsync) ? `<script src="${opts.script}"></script>` : ''
-  var favicon = opts.favicon ? `<link rel="icon" href="${opts.favicon}">` : ''
-  var css = opts.css ? `<link rel="stylesheet" href="${opts.css}">` : ''
-  var lang = opts.lang || 'en'
-  var dir = opts.dir || 'ltr'
-  var head = opts.head || ''
-  var body = opts.body || ''
+  var title = opts.title ? `<title>${opts.title}</title>` : '';
+  var headScript = (opts.script && opts.scriptAsync) ?
+	typeof(opts.script) == 'object' ?
+	opts.script.reduce((acc, path) => `${acc} <script src="${path}" async></script>`, '') :
+	`<script src="${opts.script}" async></script>` :
+	'';
+  var bodyScript = (opts.script && !opts.scriptAsync) ?
+	typeof(opts.script) == 'object' ?
+	opts.script.reduce((path, acc) => `${acc} <script src="${path}"></script>`, '') :
+	`<script src="${opts.script}"></script>` :
+	'';
+  var favicon = opts.favicon ? `<link rel="icon" href="${opts.favicon}">` : '';
+  var css = opts.css ? 
+	typeof(opts.css) == 'object' ?
+	opts.css.reduce((path, acc) => `${acc} <link rel="stylesheet" href="${path}">`, '') :
+	`<link rel="stylesheet" href="${opts.css}">` :
+	'';
+  var lang = opts.lang || 'en';
+  var dir = opts.dir || 'ltr';
+  var head = opts.head || '';
+  var body = opts.body || '';
 
   return `<!doctype html>
 <html lang="${lang}" dir="${dir}">
